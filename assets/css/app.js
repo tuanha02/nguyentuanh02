044b6b9ac5cc6bdd742b79bae5456ab8e2ebcce7
tuanha02 <110229274+tuanha02@users.noreply.github.com>
2022-08-11 20:39:41 +0700

    Add files via upload

--- assets/css/app.js
+++ assets/css/app.js
@@ -1,20 +1,19 @@
 $('.image-silder').slick({
     // dot có td làm nút bấm chuyển ảnh 12345
-    dots: true,
-    speed: 500, 
+    dots: true, 
     // fade có tác dụng gom tất cả thành một 
     fade:false ,
     // slide to show có td hiện bao nhiêu ảnh ra 
     slidesToShow: 4,
     // tos scroll là lướt một lần bao nhiêu ảnh 
-    slidesToScroll: 2,
+    slidesToScroll: 2 ,
     // infinite là chạy vô tận hoặc không chạy
-    infinite:false,
+    infinite:true,
     // arrows có td bỏ next và quay lại previous
     arrows:true,
     // autoplay silde tự chạy speeed tốc độ 1k =1s
-    autoplay:false,
-    autuplaySpeed:500,
+    autoplay:true,
+    autuplaySpeed:1000,
     // draggable có td di chuột vào k lướt sang trái phải dc (khi responsive ở điện thoại thì cần bỏ infinite và thêm draggable)
     draggable:true,
     // hai cái nút slick arrow class chung ở đây bị ẩn hay sao ấy , k cần class chung vẫn css dc 
